a1f1f83339be64fed1927b579bc35003b4847993
ellisthomas <ellisthomas80@gmail.com>
2017-04-28 22:46:25 -0500

    printing all info to dom ~ wworking on 3 day forecast

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,67 +1,94 @@
 $(document).ready(function(){
 
-const apiKey = "";
+const apiKey = "75a3c6aef353b650190a67e16e7eae3f";
+
+let myZip;
 
 // results.weather[i].description
 
 
 $("#zipBtn").on("click", (e) => {
-		let myZip = $("#usr").val();
-		loadWeather(myZip).then((result) => {
-			console.log("result", result);
+		myZip = $("#usr").val();
+		loadWeather(myZip).then((results) => {
+			console.log("results", results);
+			writeWeatherToDom(results);
 		}).catch((error) => {
 
 		});
 
 	});
 
- $('#usr').keyup(function (event) {
-        var key = event.keyCode || event.which;
-        if (key === 13) {
-            loadWeather();
+ $('#usr').keyup(function (e) {
+        if (e.keyCode === 13) {
+        	myZip = $("#usr").val();
+        	loadWeather(myZip).then((results) => {
+        		console.log("results", results);
+            writeWeatherToDom(results);
+        	}).catch((error) => {
+        		console.log(error);
+        	});
         }
-        return false;
+
     });
+
+///////////////////////////////////////////
+ const writeThreeDayDom = (results) => {
+		console.log(results);
+	};
+///////////////////////////////////////////
+
+ $("#threeDay").on("click", "a", () => {
+ 	myZip = $("#urs").val();
+ 	threeDayForecast(myZip).then((results) => {
+ 		console.log("results", results);
+ 		writeThreeDayDom(results);
+ 	}).catch((error2) => {
+ 		console.log(error2);
+ 	});
+ });
 
 
 
 	const loadWeather = (yourZip) => {
 		return new Promise((resolve, reject) => {
 			$.ajax(`http://api.openweathermap.org/data/2.5/weather?zip=${yourZip},us&units=imperial&appid=${apiKey}`)
-			.done((data) => {resolve(data);
-			console.log(data);
-			writeWeatherToDom(data);
-			writeConditionsToDom(data);
-		})
+			.done((data) => resolve(data))
 			.fail((error) => reject(error));
 		});
 	};
 
+	const threeDayForecast = (yourZip) => {
+		return new Promise((resolve, reject) => {
+			$.ajax(`api.openweathermap.org/data/2.5/forecast?zip=37211,us&appid=75a3c6aef353b650190a67e16e7eae3f`)
+			// $.ajax(`api.openweathermap.org/data/2.5/forecast?zip={yourZip},us&units=imperial&appid={apiKey}`)
+			.done((data1) => resolve(data1))
+			.fail((error) => reject(error));
+		});
+	};
 
-	// const writeWeatherToDom = (weather) => {
-	// 	let outputString = `<div>${weather}</div>`;
-	// 	$("#weather").append(outputString);
-	// };
+	
 
 
 
-
-
+	
 /// test for print ////
 
 	const writeWeatherToDom = (results) => {
 		let outputString = "";
-		for (i = 0; i < results.weather[i].length; i++){
-			outputString = `<div>${results.weather[i].description}</div>`;
+		for (let i = 0; i < results.weather.length; i++){
+			outputString += `<h1>City: ${results.name}</h1>`;
+			outputString += `<h2>Current Weather: ${results.weather[i].description}</h2>`;
 		}
+			outputString += `<h3>Temperature: ${results.main.temp}</h3>`;
+			outputString += `<h3>Air Pressure : ${results.main.pressure}</h3>`;
+			outputString += `<h3>Wind Speed: ${results.wind.speed}</h3>`;
+			outputString += `<a href="#">Three Day Forecast</a>`;
+			outputString += `<br>`;
+			outputString += `<a href="#">Seven Day Forecast</a>`;
 		$("#weather").append(outputString);
 	};
 
-	const writeConditionsToDom = (data) => {
-		let outputString = `<div>${results.main.temp}</>`;
-			outputString = `<div>${results.main.pressure}</>`;
-			$("#weather").append(outputString);
-	};
+	
 
 ///// end test /////////
 
